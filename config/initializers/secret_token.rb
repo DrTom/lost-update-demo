236f40e8303b65483c1c8bf4e3534e98605129d4
Thomas Schank <DrTom@schank.ch>
2011-11-24 14:39:15 +0100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LostUpdateDemo::Application.config.secret_token = '6f6f3b1b2ec6899cbb6140a418432b4fc291ca161c149924c58d1cd866f1e4c7cdcc4651f7e738e130465f6c003d023e0aca470afc3e44e28a35cbd447312153'
